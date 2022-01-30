d570de0ae8d89561ca9f3aa13aa6196f4fc34075
smeev653 <smeev605@gmail.com>
2022-01-30 18:36:46 +0000

    Update defines.lua

--- common/defines.lua
+++ common/defines.lua
@@ -841,7 +841,7 @@ NCountry = {
 	ANCESTOR_PERSONALITY_LEVEL_2 = 12,				-- Roof of second level of ancestor personality
 	ANCESTOR_COST = 200,							-- Cost to integrate an ancestor into the faith.
 	INITIAL_REGULAR_COLONY = 10,
-	REGULAR_COLONY_GROWTH = 35,
+	REGULAR_COLONY_GROWTH = 55,
 	COLONIAL_CLAIM_BONUS = 20,
 	COLONIAL_CLAIM_VIOLATION = 0,
 	COLONY_VIOLAION_PAPAL_INFLUENCE_COST  = 0,
@@ -903,7 +903,7 @@ NCountry = {
 	MIN_LIBERTY_DESIRE_INFLUENCE = -100,			-- Min influence of a single modifier (e.g. paid of debt)
 	MAX_LIBERTY_DESIRE_INFLUENCE = 100000,			-- Max influence of a single modifier (e.g. seized province)
 
-	MAX_CROWN_COLONIES = 9,							-- How many province a country can hold in a colonial region before creating a colonial nation
+	MAX_CROWN_COLONIES = 4,							-- How many province a country can hold in a colonial region before creating a colonial nation
 	RIVAL_TECH_THRESHOLD = 0.5,						-- Difference in tech group cost modifiers
 	OVERSEAS_DISTANCE = 150,						-- Provinces beyond this distance to capital are distant overseas
 
