73e787c1c46e0caac6851a5c3f537a2b6ff7f2d7
smeev653 <smeev605@gmail.com>
2022-10-30 13:58:04 +0000

    and this one

--- common/defines/00_dummy.lua
+++ common/defines/00_dummy.lua
@@ -273,8 +273,7 @@ NDefines.NMilitary.HUSSARS_MANPOWER_COST_MODIFIER = 1.0
 NDefines.NMilitary.HUSSARS_STARTING_STRENGTH = 1.0
 NDefines.NMilitary.HUSSARS_STARTING_MORALE = 0.1
 NDefines.NMilitary.BANNER_STARTING_STRENGTH = 0.1 -- Starting strength of a banner regiment, 1.0 being full strength
-NDefines.NMilitary.REGULAR_LEADER_POWER = 0.5
-NDefines.NMilitary.EXPLORER_LEADER_POWER = 0.4
+
 								-- Min general/admiral pips (total)
 NDefines.NMilitary.COMBAT_DICE_SIDE = 9					
 
