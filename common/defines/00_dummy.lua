92b60f255f9f8deb79e9a03e56120107f778835d
smeev653 <81537402+smeev653@users.noreply.github.com>
2022-12-25 09:59:07 +0000

    fixes

--- common/defines/00_dummy.lua
+++ common/defines/00_dummy.lua
@@ -282,8 +282,11 @@ NDefines.NMilitary.REGULAR_LEADER_POWER = 0.75
 NDefines.NMilitary.EXPLORER_LEADER_POWER = 0.65
 NDefines.NMilitary.LEADER_MAX_PIPS = 8							-- Max general/admiral pips (per skill)
 NDefines.NMilitary.LEADER_MIN_PIPS = 2
-NDefines.NMilitary.CANNOT_RETREAT_DAYS = 6
 NDefines.NMilitary.INSUFFICIENT_SUPPORT = -0.25
+NDefines.NMilitary.LEADER_MIN_PIPS = 2	
+
+								-- Min general/admiral pips (total)
+			
 
 NDefines.NAI.DIPLOMATIC_ACTION_OFFER_CONDOTTIERI_DISABLE_VERSUS_PLAYER_ENEMIES = 1
 NDefines.NAI.ACCEPTABLE_FRACTION_OF_INTEREST_PAYMENTS = 0.4
